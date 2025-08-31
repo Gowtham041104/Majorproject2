50f7b0fcdf22415021aa507411dc1dd1c8307605
Gowtham <gowthamk0411@gmail.com>
2025-08-31 19:08:58 +0530

    Initial commit 1

--- Backend/routes/chatRoutes.js
+++ Backend/routes/chatRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const {protect}= require('../middleware/authMiddleware');
-const {accessChat,sendMessage,getUserChats,getChatMessages} = require('../controllers/chatController')
-
+const { protect } = require('../middleware/authMiddleware');
+const { 
+  accessChat, 
+  sendMessage, 
+  getUserChats, 
+  getChatMessages 
+} = require('../controllers/chatController');
 const router = express.Router();
 
-router.route('/').post(protect,accessChat).get(protect,getUserChats);
-router.route('/:chatId').get(protect,getChatMessages)
-router.route('/:chatId/message').post(protect,sendMessage);
+router.route('/').post(protect, accessChat).get(protect, getUserChats);
+router.route('/:chatId').get(protect, getChatMessages);
+router.route('/:chatId/message').post(protect, sendMessage);
 
-module.exports=router
+module.exports = router;
--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,27 +1,23 @@
-const express = require("express")
-const {protect} = require("../middleware/authMiddleware")
-
+const express = require("express");
+const { protect } = require("../middleware/authMiddleware");
 const {
-    createPost,
-    getPosts,
-    createComment,
-    getPostById,
-    getUserPosts,
-    deletePost,
-    likePost,
-    unlikePost,
-} = require('../controllers/postController')
-
+  createPost,
+  getPosts,
+  createComment,
+  getPostById,
+  getUserPosts,
+  deletePost,
+  likePost,
+  unlikePost,
+} = require('../controllers/postController');
 const router = express.Router();
 
-
 // "api/posts/"
-
-router.route('/').post(protect,createPost).get(protect,getPosts);
-router.route('/:id').get(protect,getPostById).delete(protect,deletePost)
+router.route('/').post(protect, createPost).get(protect, getPosts);
+router.route('/:id').get(protect, getPostById).delete(protect, deletePost);
 router.route('/:id/like').post(protect, likePost);
 router.route('/:id/unlike').post(protect, unlikePost);
-router.route('/:id/comments').post(protect,createComment);
-router.route('/user/:userId').get(protect,getUserPosts);
+router.route('/:id/comments').post(protect, createComment);
+router.route('/user/:userId').get(protect, getUserPosts);
 
-module.exports=router
+module.exports = router;
